7a938fa71d9cb402ee9fb69c24b65480c3b9a2bd
Mihaela <mpopa62@gmail.com>
2022-07-08 18:25:45 -0400

    new code again

--- src/test/java/pages/MihaPage.java
+++ src/test/java/pages/MihaPage.java
@@ -5,5 +5,7 @@ public class MihaPage {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Anna");
+
+        System.out.println("It actually worked!");
     }
 }
