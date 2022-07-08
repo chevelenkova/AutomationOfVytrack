b2e0fd443595ef1434e162295f0f5bc716e4b89a
anna chevelenkova <chevelj@mail.ru>
2022-07-08 18:39:59 -0400

    new printout

--- src/test/java/pages/AnnaPge.java
+++ src/test/java/pages/AnnaPge.java
@@ -3,5 +3,6 @@ package pages;
 public class AnnaPge {
     public static void main(String[] args) {
         System.out.println("I hate git");
+        System.out.println("Last attempt ");
     }
 }
